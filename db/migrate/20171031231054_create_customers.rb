13ffa597b436d8753439012c88fe722a339c4339
Daniel Struthers <struthersdan@gmail.com>
2017-10-31 23:24:43 +0000

    Added categories table

---
+++ db/migrate/20171031231054_create_customers.rb
@@ -0,0 +1,16 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.foreign :province_id
+      t.string :postal_code
+      t.string :username
+      t.string :password
+      t.integer :loyalty
+
+      t.timestamps
+    end
+  end
+end
