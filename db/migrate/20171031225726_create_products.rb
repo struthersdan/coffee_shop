7e4749bbb678057f2717eceb3f2114eb5cd8ee4b
Daniel Struthers <struthersdan@gmail.com>
2017-10-31 22:57:48 +0000

    added products table

---
+++ db/migrate/20171031225726_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock_quantity
+      t.foreign :user_id
+      t.foreign :category_id
+
+      t.timestamps
+    end
+  end
+end
