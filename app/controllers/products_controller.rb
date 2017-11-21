d621b392419e0dbbf85ee317dfc3aea10556c72c
Daniel Struthers <struthersdan@gmail.com>
2017-11-21 20:42:36 +0000

    search by category, name, and description is working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @search = Product.joins(:category).ransack(params[:q])
+    @products = @search.result(distinct: true)
     @order_item = current_order.order_items.new
   end
 end
