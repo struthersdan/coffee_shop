62d212a3cafd2302cd12eea3a9884c5b63086302
Daniel Struthers <struthersdan@gmail.com>
2017-11-29 07:40:05 +0000

    cleaning migrations for heroku attempt 2

--- db/migrate/20171031225726_create_products.rb
+++ db/migrate/20171031225726_create_products.rb
@@ -7,6 +7,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.integer :stock_quantity
       t.integer :user_id
       t.integer :category_id
+      t.string  :images
 
       t.timestamps
     end
